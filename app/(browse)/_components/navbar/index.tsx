757479dc457fbae1dbaab613660742fed8f48c75
ethyashpathak <ethyashpathak@gmail.com>
2025-11-12 23:59:20 +0530

    feat(navbar): add Actions component for user authentication and dashboard access

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { Search } from "./search"
 import { Logo } from "./logo"
+import { Actions } from "./actions"
 
 
 export const Navbar=()=>{
@@ -7,6 +8,7 @@ export const Navbar=()=>{
         <nav className="fixed top-0 w-full h-20 z-49 bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm ">
             <Logo/>
             <Search/>
+            <Actions/>
         </nav>
     )
 }
