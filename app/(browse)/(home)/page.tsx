a99230f75bc8b8f06c39b59672fcad1f00b1a477
ethyashpathak <ethyashpathak@gmail.com>
2025-11-12 23:59:40 +0530

    feat(home): add UserButton component and update Home page layout

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid place-items-center h-screen">
-   <Button variant={"destructive"}>
-    Click me
-   </Button>
-   </div>
+    <div>
+      Home Page
+      </div>
   );
 }
