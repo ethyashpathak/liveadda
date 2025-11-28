72cfdb3a8b809530dbaebdd17633c12bc2373862
ethyashpathak <ethyashpathak@gmail.com>
2025-11-28 23:44:03 +0530

    feat(container): implement responsive behavior and sidebar state management in Container component

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
+
 interface ContainerProps{
     children:React.ReactNode
 }
@@ -8,8 +14,25 @@ interface ContainerProps{
 export const Container=({
     children,
 }:ContainerProps)=>{
+    const matches=useMediaQuery('(max-width:1024px)');
+    const {
+        collapsed,
+        onCollapse,
+        onExpand
+    }=useSidebar((state)=>state);
+
+    useEffect(()=>{
+        if(matches){
+            onCollapse();
+        }
+        else{
+            onExpand();
+        }
+    },[matches,onCollapse,onExpand])
+
+
     return(
-        <div>
+        <div className={cn("flex-1",collapsed ? "ml-[70px]":"ml-[70px] lg:ml-60")}>
          {children}
         </div>
     )
